bb14e24b66e08f49be447672fcfc4e1ebc68af57
Mozamel Anwary <mozamel.anwary1@gmail.com>
2020-03-07 18:15:48 +1100

    WIP label system core functional, fix listener event bugs

--- src/App.js
+++ src/App.js
@@ -3,74 +3,99 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import initColumns from './constants/InitColumns';
+import uuid from 'uuid';
+import OutsideClickHandler from 'react-outside-click-handler';
+import { 
+  columnBorderColor, 
+  columnBgColor, 
+  cardBgActiveCol, 
+  cardBgCol, 
+  delActiveCol, 
+  delCol 
+} from './constants/Colors';
+import { MentionsInput, Mention } from 'react-mentions'
+
+import KanbieLogo from './assets/kanbie-logo.svg';
 import CheckboxMarkedCircleOutlineIcon from 'mdi-react/CheckboxMarkedCircleOutlineIcon';
 import CheckboxMarkedCircleIcon from 'mdi-react/CheckboxMarkedCircleIcon';
 import ProgressCheckIcon from 'mdi-react/ProgressCheckIcon';
 import DeleteForeverIcon from 'mdi-react/DeleteForeverIcon';
 import PlusIcon from 'mdi-react/PlusIcon';
 import LabelMultipleIcon from 'mdi-react/LabelMultipleIcon';
-
-import initColumns from './constants/InitColumns';
-import KanbieLogo from './assets/kanbie-logo.svg';
-import uuid from 'uuid';
-import OutsideClickHandler from 'react-outside-click-handler';
-import { columnBorderColor, columnBgColor, cardBgActiveCol, cardBgCol, delActiveCol, delCol } from './constants/Colors';
-import { MentionsInput, Mention } from 'react-mentions'
+import CloseIcon from 'mdi-react/CloseIcon';
+import FormatColorFillIcon from 'mdi-react/FormatColorFillIcon';
 
 const maxItems = 10;
+const labelRegex = /@\[([^\]]*)\]\(([^)]*)\)/g;
 const columnIconProps = {
   size: 24,
   color: "#fff",
-  style: {marginBottom: "8px"}
+  style: { marginBottom: "8px" }
 };
 
-const labelsData = [{display: "impactio", id: "yes"}, {display: "smth", id: "no"}];
+const labelsData = [
+  { 
+    display: "impactio", 
+    id: "yes",
+    color: "#33b5e5"
+  }, 
+  { 
+    display: "wsdgwe", 
+    id: "wdwqd",
+    color: "#33b5e5"
+  }
+];
 
 function App() {
-  const [columns, setColumns] = useState();
+  const [columns, setColumns] = useState(initColumns);
   const [labels, setLabels] = useState(labelsData);
-  const [isInput, setInput] = useState(false);
+  const [currLabels, setCurrLabels] = useState({});
+  const [inputExpanded, setInputExpanded] = useState(false);
   const [inputText, setInputText] = useState("");
   const [labelsListExpanded, setLabelsListExpanded] = useState(false);
+  const [labelText, setLabelText] = useState("");
   const [loaded, setLoaded] = useState(false);
 
   // Load data
   useEffect(() => {
     let getColumns;
+    let getLabels;
     try {
       chrome.storage.sync.get("columns", function(data) {
         if (!chrome.runtime.error) {
           getColumns = data.columns;
         }
   
-        if(!getColumns || getColumns === "undefined") {
-          setColumns(initColumns);
-        } else {
+        if(getColumns && getColumns !== "undefined") {
           setColumns(getColumns);
         }
-    
-        setLoaded(true);
       });
+
+      chrome.storage.sync.get("labels", function(data) {
+        if (!chrome.runtime.error) {
+          getLabels = data.labels;
+        }
+  
+        if(getLabels && getLabels !== "undefined") {
+          setLabels(getLabels);
+        }
+      });
+
+      setLoaded(true);
     } catch (error) {
       console.warn("Error syncing with chrome extensions. Are you using this as a webapp?");
-      console.error(error);
-
-      setColumns(initColumns);
       setLoaded(true);
     }
 
-    document.addEventListener("keydown", (e) => {
-      if (e.code === "Space") {
-        handleClickAdd(); 
-      }
-    });
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, []);  
 
-  const saveColumns = (columns) => {
+  useEffect(() => {
     try {
       chrome.storage.sync.set({
-        "columns": columns
+        "columns": columns,
+        "labels": labels
       }, function() {
         if (chrome.runtime.error) {
           console.log("Runtime error. Failed to save data");
@@ -78,9 +103,26 @@ function App() {
       });
     } catch (error) {
       console.warn("Error syncing with chrome extensions. Are you using this as a webapp?");
-      console.error(error);
     }
-  }
+  }, [columns, labels])
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyShortcutHandler);
+
+    return () => {
+      document.removeEventListener('keydown', keyShortcutHandler);
+    };
+  });
+
+  const keyShortcutHandler = (e) => {
+    if (e.code === "Space" && !labelsListExpanded && !inputExpanded) {
+      setInputExpanded(!inputExpanded);
+      setTimeout(() => setInputText(""), 0);
+    } else if (e.code === "KeyL" && !labelsListExpanded && !inputExpanded) {
+      setLabelsListExpanded(!labelsListExpanded);
+      setTimeout(() => setLabelText(""), 0);
+    }
+  };
 
   const onDragEnd = (result) => {
     const { source, destination } = result;
@@ -108,51 +150,97 @@ function App() {
       }
     }
 
-    saveColumns(currColumns);
+    setColumns(currColumns);
+  }
+
+  const addCurrLabel = (id, display) => {
+    setCurrLabels({...currLabels, [id]: display});
   }
 
   const renderInputBox = () => (
     <OutsideClickHandler 
       onOutsideClick={() => {
-        setInput(false);
+        setInputExpanded(false);
         setInputText("");
+        setCurrLabels({});
       }}
     >
-      <MentionsInput 
-        value={inputText} 
-        onChange={e => setInputText(e.target.value)}
-        placeholder="New task..."
-        className="mentions input-add"
-        onKeyDown={e => onAddCard(e)}
-        autoFocus
-        maxLength={100}
-      >
-        <Mention
-          className="mentions__mention"
-          trigger="#"
-          data={labelsData}
-          displayTransform={() => ""}
-        />
-      </MentionsInput>
+      <div className="input-container">
+        <MentionsInput 
+          value={inputText} 
+          onChange={e => setInputText(e.target.value)}
+          placeholder="New task..."
+          className="mentions input-add"
+          onKeyDown={e => onAddCard(e)}
+          autoFocus
+          maxLength={100}
+          allowSpaceInQuery
+        >
+          <Mention
+            className="mentions__mention"
+            trigger="#"
+            data={labels.filter((label => !currLabels[label.id]))}
+            displayTransform={() => ""}
+            onAdd={(id, display) => addCurrLabel(id, display)}
+          />
+        </MentionsInput>
+        {!!Object.values(currLabels).length && 
+          <div className="curr-labels-container">
+            {Object.keys(currLabels).map((keyLabel, i) => (
+              <span 
+                className="curr-label-item"
+                style={{backgroundColor: labels.find(label => label.id === keyLabel).color}}
+                key={keyLabel}
+              >
+                {currLabels[keyLabel]}
+              </span>
+            ))}
+          </div>
+        }
+      </div>
     </OutsideClickHandler>
   )
 
   const renderLabelsList = () => (
-    <OutsideClickHandler onOutsideClick={() => setLabelsListExpanded(false)}>
+    <OutsideClickHandler onOutsideClick={() => setLabelsListExpanded(!labelsListExpanded)}>
       <div className="labels-list">
+        <input 
+          className="mentions__input"
+          style={{backgroundColor: "transparent"}}
+          placeholder="Add label..."
+          maxLength={24}
+          autoFocus
+          value={labelText}
+          onChange={e => setLabelText(e.target.value)}
+          onKeyDown={e => onAddLabel(e)}
+        />
         {labels.map(label => (
           <div className="label-item"> 
-            {label.display}
+            <span 
+              className="label-item-text" 
+              style={{backgroundColor: label.color}}
+            >
+                {label.display}
+            </span>
+            <span className="label-item-action-container">
+              <FormatColorFillIcon 
+                onClick={() => console.log("change col")} 
+                style={{color: "grey", marginTop: "6px"}}
+              />
+              <CloseIcon 
+                onClick={() => deleteLabel(label.id)} 
+                style={{color: "grey"}}
+              />
+            </span>
           </div>
         ))}
       </div>
     </OutsideClickHandler>
   )
 
-  const handleClickAdd = () => {
-    if(!isInput) {
-      setInput(true);
-    }
+  const deleteLabel = (labelId) => {
+    const remainingLabels = [...labels].filter(label => label.id !== labelId);
+    setLabels(remainingLabels);
   }
 
   const onAddCard = (e) => {
@@ -162,12 +250,48 @@ function App() {
         content: inputText
       });
 
-      setInput(false);
-      saveColumns(columns);
+      setInputExpanded(false);
       setInputText("");
+      setCurrLabels({});
     }
+  }
 
-    return;
+  const onAddLabel = (e) => {
+    if(e.key === "Enter" && labelText !== "") {
+      labels.push({
+        id: uuid(),
+        display: labelText,
+        color: "#33b5e5"
+      });
+
+      setLabelText("");
+    }
+  }
+
+  const getLabelsFromCard = (cardText) => {
+    const filteredLabels = cardText.match(labelRegex);
+    if(!filteredLabels) return;
+
+    return (
+      <div className="card-label-container">
+        {filteredLabels.reverse().map(filteredLabel => (
+          labels.find(label => 
+            label.id === filteredLabel.replace(labelRegex, "$2")
+          ) && 
+            <span 
+              key={filteredLabel}
+              className="card-label-item"
+              style={{backgroundColor: 
+                labels.find(label => 
+                  label.id === filteredLabel.replace(labelRegex, "$2")
+                )?.color || "#000"
+              }}
+            >
+              {filteredLabel.replace(labelRegex, "$1")}
+            </span>
+          ))}
+      </div>
+    );
   }
 
   return (
@@ -184,7 +308,7 @@ function App() {
           onDragEnd={onDragEnd}
         >
           {columns.map(column => (
-            <div>
+            <div key={column.title}>
               <div className="column-header-container">
                 <div className="column-heading">{column.title}</div>
                 {column.title === "To-do" && <CheckboxMarkedCircleOutlineIcon {...columnIconProps}/>}
@@ -217,7 +341,8 @@ function App() {
                             }}
                             className="draggable-card"
                           >
-                            {item.content}
+                            {item.content.replace(labelRegex, '')}
+                            {getLabelsFromCard(item.content)}
                           </div>
                         )}
                       </Draggable>
@@ -246,18 +371,19 @@ function App() {
         </DragDropContext>
         <div 
           className="add-button-container droppable-container"
-          onClick={() => handleClickAdd()}
+          onClick={() => setInputExpanded(!inputExpanded)}
         >
           <PlusIcon color="#fff" className="add-icon"/>
-          {!!isInput && renderInputBox()}
         </div>
+        
         <div 
           className="labels-button-container droppable-container"
           onClick={() => setLabelsListExpanded(!labelsListExpanded)}
         >
           <LabelMultipleIcon color="#fff" className="add-icon"/>
-          {!!labelsListExpanded && renderLabelsList()}
         </div>
+        {!!inputExpanded && renderInputBox()}
+        {!!labelsListExpanded && renderLabelsList()}
       </div>
       }
     </div>
