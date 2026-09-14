02186f610e65708ef98c11cef162ca6b4c4cdd3c
Mozamel Anwary <mozamel.anwary1@gmail.com>
2026-09-15 00:16:13 +1000

    label ui fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -31,6 +31,7 @@ export function Card({
   if (filteredLabels && labels) {
     const seen = new Set();
     for (const match of filteredLabels) {
+    for (const match of [...filteredLabels].reverse()) {
       const labelId = match.replace(labelRegex, '$2');
       if (!seen.has(labelId)) {
         seen.add(labelId);
